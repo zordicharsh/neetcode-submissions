3a82782a4560f6f56002adb31a56891cb5c38bf7
zordicharsh <111136960+zordicharsh@users.noreply.github.com>
2026-07-15 23:08:44 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+
+        for(int i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch) - 1);
+            }else{
+                return false;
+            }
+        }
+
+        for (int value : map.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
